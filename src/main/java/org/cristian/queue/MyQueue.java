ce2a6b13671d290be747a9aa2e7a45f853902c23
cristian <cristian.c.lopez.m@gmail.com>
2024-01-08 20:32:09 -0500

    Queue

--- src/main/java/org/cristian/queue/MyQueue.java
+++ src/main/java/org/cristian/queue/MyQueue.java
@@ -1,4 +1,29 @@
 package org.cristian.queue;
 
+import java.util.PriorityQueue;
+
 public class MyQueue {
+
+//    PriorityQueue genericQueue = new PriorityQueue(); // Not allowed
+    static PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+        addElements();
+        System.out.println(queue);
+        System.out.println("head: " + queue.element()); // can return an exception
+        System.out.println("head: " + queue.peek()); // can return a null value
+        queue.remove(); // can return an exception
+        queue.poll(); // can return a null value
+        System.out.println(queue);
+    }
+
+    static void addElements(){
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        queue.offer(6); // return true if is successfully if isn't successfully is null.
+    }
+
 }
---
+++ src/main/java/org/cristian/queue/QUEUE.md
@@ -0,0 +1,11 @@
+# Queue 
+
+Queue has two classes:
+
+* PriorityQueue
+* LinkedList <- also extends from List Interface
+
+Queue is based on the concept FIFO; it means "First in First out."
+
+> [!IMPORTANT]
+> Queue just allow homogeneous data.
